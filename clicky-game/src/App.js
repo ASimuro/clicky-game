5d5ccd281cf45204326f2fc09a90da6ea8ffd576
Alex Simuro <aasimuro12@gmail.com>
2019-11-20 18:07:54 -0500

    json file

---
+++ clicky-game/src/App.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import MatchCard from "./components/Body";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import matches from "./images.json";
